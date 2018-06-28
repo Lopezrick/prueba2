9f7e3a864f60b1135d551b5a321f52abb9b8e792
LopezMenjivar <rikrdo.mejivar@gmail.com>
2018-06-28 17:13:43 -0600

    #6

--- src/prestamos/Prestamos.java
+++ src/prestamos/Prestamos.java
@@ -24,8 +24,23 @@ public class Prestamos {
       public ResultSet Articulos() {
         return cn.getValores("SELECT*FROM articulos");
     }
+       public ResultSet codigosPrestamo() {
+        return cn.getValores("SELECT*FROM detalleprestamo ");
+    }
       public void agregarArticulo(int id, String descripcion) {
         cn.UID("INSERT INTO articulos(idArticulo,descripcion) VALUES('" + id + "','" + descripcion + "')");
         
+    }
+      public ResultSet contarRegistros() {
+        return (cn.getValores("SELECT COUNT(codPrestamo) FROM detalleprestamo"));
+    }
+      public void AgregarDevolucion(String codigo, String fecha) {
+        cn.UID("UPDATE detalleprestamo SET fechaDevolucion='" + fecha + "'  WHERE codPrestamo='" + codigo + "'");
+    }
+      public ResultSet mostrarTodos() {
+        return (cn.getValores(" SELECT detalleprestamo.codPrestamo, alumnos.carnet, articulos.descripcion, detalleprestamo.cantidad, detalleprestamo.fechaPedido, detalleprestamo.fechaDevolucion FROM articulos INNER JOIN (alumnos INNER JOIN detalleprestamo ON alumnos.carnet = detalleprestamo.carnet) ON articulos.idArticulo = detalleprestamo.idArticulo"));
+    }
+      public ResultSet buscarporCarnet(String carnet) {
+        return (cn.getValores("SELECT detalleprestamo.codPrestamo, alumnos.carnet, articulos.descripcion, detalleprestamo.cantidad, detalleprestamo.fechaPedido, detalleprestamo.fechaDevolucion FROM articulos INNER JOIN (alumnos INNER JOIN detalleprestamo ON alumnos.carnet =  detalleprestamo.carnet) ON articulos.idArticulo = detalleprestamo.idArticulo WHERE (((alumnos.carnet)='"+carnet+"'));"));
     }
 }
